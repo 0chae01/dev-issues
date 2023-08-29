24cc2eb1d419ac4bc2be456aaec92f3dd0dfc8aa
youngchae <010101chae@gmail.com>
2023-08-29 23:54:33 +0900

    chore: octokit 설치, feat: RouterProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
 
 export default App;
